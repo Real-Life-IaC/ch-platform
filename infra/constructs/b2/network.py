f7815d3e9edf662b7f7e2f8e6f3f1fdc77dc62b5
Andre Sionek <andresionek91@gmail.com>
2025-01-03 18:43:35 +0000

    fix: reduce nat costs

--- infra/constructs/b2/network.py
+++ infra/constructs/b2/network.py
@@ -1,7 +1,8 @@
 from aws_cdk import aws_ec2 as ec2
 from aws_cdk import aws_ssm as ssm
+from cdk_fck_nat import FckNatInstanceProvider
 from constructs import Construct
-import aws_cdk as cdk
+
 
 class B2Network(Construct):
     """Create a VPC and Subnets following specific allocations"""
@@ -16,16 +17,29 @@ class B2Network(Construct):
     ) -> None:
         super().__init__(scope, id)
 
+        nat_provider = FckNatInstanceProvider(
+            instance_type=ec2.InstanceType.of(
+                ec2.InstanceClass.T4G, ec2.InstanceSize.NANO
+            ),
+            enable_cloud_watch=True,
+            enable_ssm=True,
+        )
+
         # Create the VPC using the L2 construct
         self.vpc = ec2.Vpc(
             scope=self,
             id="Vpc",
             max_azs=max_azs,
-            nat_gateways=nat_gateways,
+            nat_gateway_provider=nat_provider,
             subnet_configuration=self.subnet_configuration,
             ip_addresses=ec2.IpAddresses.cidr(cidr_block=cidr_block),
             enable_dns_hostnames=True,
             enable_dns_support=True,
+        )
+
+        nat_provider.security_group.add_ingress_rule(
+            peer=ec2.Peer.ipv4(self.vpc.vpc_cidr_block),
+            connection=ec2.Port.all_traffic(),
         )
 
         # Add a flow log to the VPC
